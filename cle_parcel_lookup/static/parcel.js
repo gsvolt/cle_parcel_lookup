30e40aea817f09df781f3deed25a61e4648fb74b
gsvolt <gsvolt7@gmail.com>
2020-10-12 15:49:21 -0400

    Added approximate addresses based on reverse geocoding google maps api, visual changes

--- cle_parcel_lookup/static/parcel.js
+++ cle_parcel_lookup/static/parcel.js
@@ -8,11 +8,29 @@ function updateData() {
 	});
 }
 
+const apiKey = 'APIKEY';
+
 function loadMap(latitude, longitude) {
-	const apiKey = 'APIKEY';
 	let mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${latitude},${longitude}`;
 	map_frame=`<iframe width="500" height="375" frameborder="0" style="border:0" src="${mapUrl}" allowfullscreen></iframe>`;
 	$('#map').html(map_frame);
+}
+
+function reverseLookup(latitude, longitude) {
+    let mapUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&latlng=${latitude},${longitude}`;
+    $.ajax({
+        url: mapUrl
+    }).done(function(data){
+        let formattedAddressesHtml = '<ul>';
+        for (i = 0; i < data.results.length; i++) {
+            formattedAddressesHtml += `<li class="formatted_address"><a href="https://www.google.com/maps?q=${data.results[i].formatted_address}" target="_blank">${data.results[i].formatted_address}</a></li>`;
+            if (data.results[i].formatted_address.includes('-')) {
+                break;
+            }
+        }
+        formattedAddressesHtml += '</ul>';
+        $('#reverseLookup').html(formattedAddressesHtml);
+    });
 }
 
 function getHtml(responseJson) {
@@ -29,29 +47,30 @@ function getHtml(responseJson) {
         html += '</div>';
 
         if(record) {
-                html += '<div class="row">';
-                html += '<div class="col header">Name</div>';
-                html += '<div class="col header">Description</div>';
-                html += '<div class="col header">Value</div>';
-                html += '<div class="col-sm header">Update Date</div>';
+            html += '<div class="row">';
+            html += '<div class="col header">Name</div>';
+            html += '<div class="col header">Description</div>';
+            html += '<div class="col header">Value</div>';
+            html += '<div class="col-sm header">Update Date</div>';
+            html += '<div class="w-100"></div>';
+            for(const [key,value] of Object.entries(record)){
+                html += `<div class="col rowData">${key}</div>`;
+                html += `<div class="col rowData">${value.description ? value.description : ''}</div>`;
+                html += `<div class="col rowData">${value.value ? value.value : ''}</div>`;
+                html += `<div class="col-sm rowData">${value.update_date}</div>`;
                 html += '<div class="w-100"></div>';
-                for(const [key,value] of Object.entries(record)){
-                        html += `<div class="col">${key}</div>`;
-                        html += `<div class="col">${value.description ? value.description : ''}</div>`;
-                        html += `<div class="col">${value.value ? value.value : ''}</div>`;
-                        html += `<div class="col-sm">${value.update_date}</div>`;
-                        html += '<div class="w-100"></div>';
-                        if(key === 'LATITUDE') {
-                                latitude = value.value;
-                        }
-                        if(key === 'LONGITUDE') {
-                                longitude = value.value;
-                        }
-                } 
-                html += '</div>';
+                if(key === 'LATITUDE') {
+                    latitude = value.value;
+                }
+                if(key === 'LONGITUDE') {
+                    longitude = value.value;
+                }
+            } 
+            html += '</div>';
         }
 	});
-	loadMap(latitude, longitude);
+    loadMap(latitude, longitude);
+    reverseLookup(latitude, longitude);
 	return html;
 }
 
